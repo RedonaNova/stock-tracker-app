4d559661accf7147ef33bdb51c70b043358e394f
ra <radnaa2015@gmail.com>
2025-11-02 11:47:26 +0800

    implement header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  );
+  return <div className="flex min-h-screen home-wrapper">Home</div>;
 }
